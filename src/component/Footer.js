4aa2d586e94bb3976dd22c9b1a08f94b4a309bb9
akibbhuiyan <akibbhh@gmail.com>
2023-03-10 19:59:20 +0600

    query

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -6,13 +6,13 @@ import { FaGithub } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className="footer">
-      <Container className="pt-4">
+      <Container className="pt-4 ">
         <MailChimpForm />
         <Row className="align-items-center">
           <Col sm={6}>
             <h1>Akib Bhuiyan</h1>
           </Col>
-          <Col sm={6} className="text-center text-sm-end">
+          <Col sm={12} md={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="https://www.linkedin.com/in/akib-bhuiyan-878778252/">
                 <ImLinkedin2 />
